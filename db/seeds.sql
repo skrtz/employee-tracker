2686c757892abf187e8bf574d84e0825a0bc0369
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-05-20 19:08:33 -0400

    update seeds to include role id's and manager id's

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,9 +3,8 @@ USE company_db;
 INSERT INTO departments (name)
 VALUES ("Sales"), ("Engineer"), ("Finance"), ("Lawyer");
 
-INSERT INTO roles (title, salary)
-VALUES ("Sales Lead", 80000), ("Sales Person", 50000), ("Lead Engineer", 150000), ("Software Engineer", 100000), ("Acount Manager", 100000), ("Accountant", 75000), ("Legal", 150000);
+INSERT INTO roles (title, salary, dept_id)
+VALUES ("Sales Lead", 80000, 1), ("Sales Person", 50000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 100000, 2), ("Acount Manager", 100000, 3), ("Accountant", 75000, 3), ("Legal", 150000, 4);
 
-INSERT INTO employees (first_name, last_name)
-VALUES ("Paulie", "Walnuts"), ("Christopher", "Moltisanti"), ("Tony", "Saprono"), ("Silvio", "Dante"), ("Hesh", "Rabkin"), ("Furio", "Giunta"), ("Neil", "Mink");
- 
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Paulie", "Walnuts", 1), ("Christopher", "Moltisanti", 2, 1), ("Tony", "Saprono", 3), ("Silvio", "Dante", 4, 3), ("Hesh", "Rabkin", 5), ("Furio", "Giunta", 6, 5), ("Neil", "Mink", 7);
