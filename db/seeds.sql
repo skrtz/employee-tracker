2d6dca9fe59cd94dc4dd791bd46e04d0c5a85121
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-05-20 18:20:44 -0400

    add seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,11 @@
 USE company_db;
 
+INSERT INTO departments (name)
+VALUES ("Sales"), ("Engineer"), ("Finance"), ("Lawyer");
+
+INSERT INTO roles (title, salary)
+VALUES ("Sales Lead", 80000), ("Sales Person", 50000), ("Lead Engineer", 150000), ("Software Engineer", 100000), ("Acount Manager", 100000), ("Accountant", 75000), ("Legal", 150000);
+
+INSERT INTO employees (first_name, last_name)
+VALUES ("Paulie", "Walnuts"), ("Christopher", "Moltisanti"), ("Tony", "Saprono"), ("Silvio", "Dante"), ("Hesh", "Rabkin"), ("Furio", "Giunta"), ("Neil", "Mink");
+ 
